5acf2f351f1d1a45924cd7dcdfb254e523ac7e91
Murray <code@murraycox.com>
2014-03-09 14:38:20 -0400

    Change toolbars to external toolbar
    Fix flickering on page navigation

--- js/mysisterskeeper.js
+++ js/mysisterskeeper.js
@@ -1,12 +1,35 @@
-        $(document).on("pagecreate", "#askq",  function(event){
-            $("#sendQuestion").click(function(){
-                $.mobile.loading( "show", {
-                    text: "",
-                    textVisible: true,
-                    textonly: false,
-                    theme: "b",
-                    html: "<h3>Sent!</h3><p>Thanks for your question! We'll let you know when we add it to the app.</p>"
-                });
-                setTimeout(function(){ $.mobile.loading( "hide" ); }, 5000);
-            });
+$(document).on("pagecreate", "#askq",  function(event){
+    $("#sendQuestion").click(function(){
+        $.mobile.loading( "show", {
+            text: "",
+            textVisible: true,
+            textonly: false,
+            theme: "b",
+            html: "<h3>Sent!</h3><p>Thanks for your question! We'll let you know when we add it to the app.</p>"
         });
+        setTimeout(function(){ $.mobile.loading( "hide" ); }, 5000);
+    });
+});
+
+$(function(){
+	$( "[data-role='footer']" ).toolbar({ theme: "a" });
+});
+
+
+// Update the contents of the toolbars
+$( document ).on( "pageshow", "[data-role='page']", function() {
+    // Each of thepages has a data-title attribute
+    // which value is equal to the text of the nav button
+    // For example, on first page: <div data-role="page" data-title="Home">
+    var current = $( this ).jqmData( "title" );
+    // Change the heading
+    $( "[data-role='header'] h1" ).text( current );
+    // Remove active class from nav buttons
+    $( "[data-role='navbar'] a.ui-btn-active" ).removeClass( "ui-btn-active" );
+    // Add active class to current nav button
+    $( "[data-role='navbar'] a" ).each(function() {
+        if ( $( this ).text() === current ) {
+            $( this ).addClass( "ui-btn-active" );
+        }
+    });
+});
