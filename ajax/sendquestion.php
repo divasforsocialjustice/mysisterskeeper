829a20ff874a8f60502e575ce01fc34934e22243
Murray <code@murraycox.com>
2014-03-22 23:52:20 -0400

    Update e-mail address to diva4justice

--- ajax/sendquestion.php
+++ ajax/sendquestion.php
@@ -5,7 +5,7 @@
 
     //send the email
     $body = "Someone just submitted a question for the App\n" . "Name: " . $_POST['sendQuestion_Name'] . "\nE-Mail: " . $_POST['sendQuestion_Email'] . "\nQuestion:\n" . $_POST['sendQuestion_Question'] . "\n";
-    mail("mysisterskeeper@murraycox.com, mysisterskeeper2@murraycox.com", "My Sister's Keeper App - New Question", $body);
+    mail("mysisterskeeper@murraycox.com, diva4justice@gmail.com", "My Sister's Keeper App - New Question", $body);
 
     //set the response
     $response_array['status'] = 'success';
